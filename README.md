8b92b8177cac675f815e3684e32818c279da68da
DavMSmith <davonda.smith@gmail.com>
2021-09-22 21:03:34 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+      Todo
+      title-string
+      complete-boolean
+Todo has many comments
+comment belong to a todo
+
+      Comment
+        title-string
+        body-text
+        author-string
+    Note: These are all backend components
---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,9 @@
+class Api::TodosController < ApplicationController
+  # only the CRUD actions
+  # new and edit are no longer here
+  #new and edit form are in the react side now
+  # index, show, create, update, destroy belong here
+  # redirect_to no longer belongs here
+  #return data, html, xml, json
+  #json object notation for passing data 
+end
